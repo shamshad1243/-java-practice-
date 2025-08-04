c7fc3677e18381e173b8c6777f066211dd63ebb2
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 16:58:05 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/Integer.java
@@ -0,0 +1,20 @@
+package Java_Practice_Solutions;
+
+public class Integer {
+
+        public static void main(String[] args){
+            int a=100000;
+            // -2,147,483,648 to 2,147,483,647
+            short s=4000;
+            // -32768 to32768
+            byte c=120;
+            // -128 to 127
+            long d=10000;
+            //-9223372036854775808 to 9223372036854775807
+            System.out.println("the integer variable is "+a+'\n');
+            System.out.println("the short variable is "+s+'\n');
+            System.out.println("the byte variable is "+c+'\n');
+            System.out.println("the long variable is "+d+'\n');
+        }
+    }
+
