84ae2dd56402dc27cde425d6e7fc68012bed0de1
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 16:52:16 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/FloatVariable.java
@@ -0,0 +1,16 @@
+package Java_Practice_Solutions;
+
+public class FloatVariable {
+
+
+        public static void main(String[] ages)
+        {
+            float f=87249838434f;
+            //1.4E-45 to 3.4028235E38
+            double d=100399837333d;
+            //4.9E-324 to 1.7976931348623157E308
+            System.out.println(" the float variable is "+f+'\n');
+            System.out.println(" the double variable is "+d+'\n');
+
+        }
+    }
