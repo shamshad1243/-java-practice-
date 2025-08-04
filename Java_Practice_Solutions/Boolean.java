877b64989525a2d2562daff3ad4946e0e68f14e4
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 17:04:37 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/Boolean.java
@@ -0,0 +1,17 @@
+package Java_Practice_Solutions;
+
+public class Boolean {
+
+        public static void main(String[] arge)
+        {
+            boolean var1 =true;
+            if (!var1) {
+                System.out.println("boolean value is false");
+            } else {
+                System.out.println("boolean value is ture");
+            }
+        }
+    }
+
+
+
