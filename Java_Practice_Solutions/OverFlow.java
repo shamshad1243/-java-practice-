ed4f2568163ce7cc035bdea07e2694911a34f76d
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-04 17:09:12 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/OverFlow.java
@@ -0,0 +1,15 @@
+package Java_Practice_Solutions;
+
+public class OverFlow {
+
+        public static void main(String[] arge){
+            // overflow example
+            int a=130;
+            byte b=(byte)a;
+
+            System.out.println(a);
+
+            System.out.println(b);
+
+        }
+    }
