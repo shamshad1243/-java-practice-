497e7ad210538038b39f0098e94b40a934d6c374
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-05 18:56:35 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/Loop3.java
@@ -0,0 +1,15 @@
+package Java_Practice_Solutions;
+
+public class Loop3 {
+    public static void main(String[] arge) {
+        int i = 0;
+        do {
+            {
+                System.out.println(i);
+                i++;
+
+            }
+        }       while(i<=10);
+
+    }
+}
