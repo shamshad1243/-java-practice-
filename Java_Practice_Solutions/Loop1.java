06acd2eba1c53c7de45cf779505f3c947f15b42c
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-05 18:42:54 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/Loop1.java
@@ -0,0 +1,12 @@
+package Java_Practice_Solutions;
+
+public class Loop1 {
+public static void main( String[] arge ){
+    int i=0;
+    while ( i<=10)
+    {
+        System.out.println(i +" ");
+        i++;
+    }
+}
+}
