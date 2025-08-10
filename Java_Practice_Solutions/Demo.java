458a2be2c203bee9ac7ef90af76b7d20474d7d16
Shamshad Ansari <shamshadansari43984@gmail.com>
2025-08-10 17:46:06 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/Demo.java
@@ -0,0 +1,21 @@
+package Java_Practice_Solutions;
+
+public class Demo {
+
+        int a=10;
+        String b="shadab";
+        void show()
+        {
+            System.out.println(a+"."+b);
+
+        }
+    }
+    class Test
+    {
+        public static void main(String[] arge )
+        {
+            Demo r= new Demo();
+            r.show();
+        }
+    }
+
