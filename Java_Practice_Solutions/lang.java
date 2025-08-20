b7cf7114c4ab8fa5cc27b502808d742e8fcc7f46
Shamshad <shamshadansari43984@gmail.com>
2025-08-20 18:27:04 +0530

    Add files via upload

---
+++ Java_Practice_Solutions/lang.java
@@ -0,0 +1,10 @@
+public class lang {
+    public static void main(String[] args) {
+        String s = "Hello Java";
+
+        System.out.println(s.toUpperCase());
+        // java.lang.String class
+        System.out.println(Math.sqrt(25));
+        // java.lang.Math class
+    }
+}
