6684d9652ffa81949cf63fd68162959ec0725f9b
Shamshad <shamshadansari43984@gmail.com>
2025-08-27 19:44:43 +0530

    Add files via upload

---
+++ inputoroutput.java
@@ -0,0 +1,12 @@
+package Java_Practice_Solutions;
+import java.util.Scanner;
+
+ class A {
+     public static void main(String[] args) {
+          int a ;
+         System.out.println(" Enter data ");
+         Scanner obj  = new Scanner(System.in);
+         a = obj.nextInt();
+         System.out.println(" get data " + a);
+     }
+ }
