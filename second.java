0c3085e963b5d8a82dd6f9078d75d09f62868c62
Shamshad <shamshadansari43984@gmail.com>
2025-10-01 16:53:51 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,16 @@
+package heroQN;
+
+public class second {
+    int a= 10;
+    static double b=20.5;
+    public static void main (String Args[])
+    {
+        boolean c = true ;
+        // local variable
+        second s = new second();
+       // System.out.println(s.a+" " +b+" "+c);
+        System.out.println(s.a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
